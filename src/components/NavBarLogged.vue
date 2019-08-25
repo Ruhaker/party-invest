38bbd718f22c4c107f05a982ec10dbcaf3693370
Ruhaker <lhby2389@gmail.com>
2019-08-25 04:59:34 -0300

    Add profile page

---
+++ src/components/NavBarLogged.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>party invest</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-text>Fernando Zhin</v-text>
+      </v-toolbar-items>
+
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vuetify'
+}
+</script>
---
+++ src/components/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-content>
+      <NavBarLogged />
+      
+      <v-container class="white" bg fill-height grid-list-md text-xs-center>
+        <v-row no-gutters>
+          <v-col
+            v-for="n in 3"
+            :key="n"
+            cols="sm"
+          >
+            <v-card
+              class="pa-10"
+              outlined
+              tile
+            >
+              One of three columns
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavBarLogged from './NavBarLogged';
+
+export default {
+  name: 'Profile',
+  components: {
+    NavBarLogged
+  }
+};
+
+</script>
+
+<style>
+
+</style>
