6507daf77c7e31ce4bcbe79a04dd2fd87753c94f
Ruhaker <lhby2389@gmail.com>
2019-08-25 02:54:55 -0300

    Add login frontend

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,29 +1,41 @@
 <template>
   <v-app>
     <v-content>
-      <NavBar/>
-        <v-card
-          max-width="344"
-          class="mx-auto"
-        >
-          <v-card-title>I'm a title</v-card-title>
-          <v-card-text>I'm card text</v-card-text>
-          <v-card-actions>
-            <v-btn text>Click</v-btn>
-          </v-card-actions>
-        </v-card>
+      <v-container bg fill-height grid-list-md text-xs-center>
+        <v-layout row wrap align-center>
+          <v-flex>
+            <v-card 
+              max-width="344"
+              class="mx-auto"
+            >
+              <v-card-title>Sign in</v-card-title>
+              <v-card-text>
+                <v-text-field
+                  label="Email"
+                  placeholder="@email.com"
+                ></v-text-field>
+                <v-text-field
+                  label="Senha"
+                  placeholder="Senha"
+                ></v-text-field>
+              </v-card-text>
+              <v-card-actions>
+                <router-link to="/profile">
+                  <v-btn text>Enter</v-btn>
+                </router-link>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import NavBar from './NavBar';
 
 export default {
   name: 'Profile',
-  components: {
-    NavBar
-  },
   data: () => ({
     //
   }),
