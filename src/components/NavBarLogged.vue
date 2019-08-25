1894425c5390921f7f4b2ab46fc74f5ef6097361
Ruhaker <lhby2389@gmail.com>
2019-08-25 06:09:30 -0300

    Fix 'patrocíneo'

--- src/components/NavBarLogged.vue
+++ src/components/NavBarLogged.vue
@@ -6,7 +6,14 @@
       <div class="flex-grow-1"></div>
 
       <v-toolbar-items>
-        <v-text>Fernando Zhin</v-text>
+        <v-btn text to="/FindInvestor">Patrocinadores</v-btn>
+        <v-btn text to="/FindProject">Projetos</v-btn>
+        <div> cm-admin </div>
+        <template v-if="$vuetify.breakpoint.smAndUp">
+          <v-btn icon to="/">
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </template>
       </v-toolbar-items>
 
     </v-toolbar>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,25 +2,9 @@
   <v-app>
     <v-content>
       <NavBarLogged />
-      
-      <v-container class="white" bg fill-height grid-list-md text-xs-center>
-        <v-row no-gutters>
-          <v-col
-            v-for="n in 3"
-            :key="n"
-            cols="sm"
-          >
-            <v-card
-              class="pa-10"
-              outlined
-              tile
-            >
-              One of three columns
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-
+        <center>
+          <img src="../assets/perfil.png">
+        </center>
     </v-content>
   </v-app>
 </template>
