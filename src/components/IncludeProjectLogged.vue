5a8eea292d1e7d6c74dc2c192efcb0c4af78e5ab
lucasns97 <lucasns97@hotmail.com>
2019-08-25 08:42:48 -0300

    km

---
+++ src/components/IncludeProjectLogged.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-content>
+      <NavBarLogged />
+      <section id="white" style="padding-top: 50px">
+        <center>
+          <h1 class="black--text mb-2 display-1 text-center">
+              Workshop InovaUSP
+              </h1>
+        <img src="../assets/detalhes_projeto.png">   
+        </center> 
+      </section>
+      <section id="white" style="padding-top: 50px; padding-bottom: 70px">
+        <center>
+          <h1 class="black--text mb-2 display-1 text-center">
+              Patrocinadores
+              </h1>
+        <img src="../assets/patrocineos.png">   
+        </center> 
+      </section>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavBarLogged from './NavBarLogged';
+
+export default {
+  name: 'IncludeProjectLogged',
+  components: {
+    NavBarLogged
+  }
+};
+
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import FindInvestor from './components/FindInvestor.vue';
 import FindProject from './components/FindProject.vue';
 import Sponsors from './components/Sponsors.vue'
 import IncludeProject from './components/IncludeProject.vue'
+import HomeLogged from './components/HomeLogged';
+import FindInvestorLogged from './components/FindInvestorLogged.vue';
+import FindProjectLogged from './components/FindProjectLogged.vue';
+import IncludeProjectLogged from './components/IncludeProjectLogged.vue'
 
 // import routes from './routes';
 
@@ -20,12 +24,16 @@ Vue.use(VueRouter)
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/Logged', component: HomeLogged },
     { path: '/login', component: Login },
     { path: '/profile', component: Profile },
     { path: '/FindInvestor', component: FindInvestor },
+    { path: '/FindInvestorLogged', component: FindInvestorLogged },
     { path: '/FindProject', component: FindProject },
+    { path: '/FindProjectLogged', component: FindProjectLogged },
     { path: '/Sponsors', component: Sponsors },
-    { path: '/IncludeProject', component: IncludeProject }
+    { path: '/IncludeProject', component: IncludeProject },
+    { path: '/IncludeProjectLogged', component: IncludeProjectLogged }
 			
 ];
 console.log('passou aqui')
