58f9d8dc8e2dfaaa355c964aef3ea158c6067581
lucasns97 <lucasns97@hotmail.com>
2019-08-25 08:00:17 -0300

    img

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,9 +2,14 @@
   <v-app>
     <v-content>
       <NavBarLogged />
+      <section id = 'white' style="padding-top: 50px">
         <center>
+          <h1 class="black--text mb-2 display-1 text-center">
+              Perfil
+              </h1>
           <img src="../assets/perfil.png">
         </center>
+        </section>
     </v-content>
   </v-app>
 </template>
