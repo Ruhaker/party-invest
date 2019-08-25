4626c32254283ebd9311bd65b58fba08bec81867
gabrielmorete <gabrielmorete@usp.br>
2019-08-25 02:42:06 -0300

    corrigindo main.js e add outras pgs

---
+++ src/components/FindInvestor.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-content>
+      <NavBar/>
+
+    </v-content>
+  </v-app>
+     
+</template>
+
+<script>
+import NavBar from './NavBar';
+import Footer from './Footer';
+
+
+export default {
+  name: 'FindInvestor',
+  components: {
+    NavBar,
+    Footer
+  },
+  data: () => ({
+    //
+  }),
+};
+
+</script>
+
+<style>
+  .v-content{
+    background-color: #03a5fc;
+  }
+  #white{
+    background-color: white;
+  }
+  #logo{
+    float: right;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+  }
+  
+</style>
